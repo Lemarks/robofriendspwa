17e897357c0c65c25e414d35a88ddc1e0266bee9
Lemarks <losei1@umbc.edu>
2018-08-09 20:17:17 -0400

    503 compliance and lighthouse score improve

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({
     }) => {
 return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow'>
-        <img alt='robots ' src={`https://robohash.org/${id}}?200x200`} />
+        <img alt='robots ' src={`https://robohash.org/${id}}?size=200x200`} />
         <div>
             <h2>{name}</h2>
             <p>{email}</p>
